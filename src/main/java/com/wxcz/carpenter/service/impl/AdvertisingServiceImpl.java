ecfc1dfc11a9a5992e6a41f28bb826e663a48db4
chenxi1280 <chenxi1280@outlook.com>
2021-05-20 13:24:33 +0800

    banner广告加入

--- src/main/java/com/wxcz/carpenter/service/impl/AdvertisingServiceImpl.java
+++ src/main/java/com/wxcz/carpenter/service/impl/AdvertisingServiceImpl.java
@@ -1,8 +1,10 @@
 package com.wxcz.carpenter.service.impl;
 
 import com.wxcz.carpenter.dao.EcmAdSettingsDao;
+import com.wxcz.carpenter.dao.EcmBannerDao;
 import com.wxcz.carpenter.pojo.dto.PageDTO;
 import com.wxcz.carpenter.pojo.dto.ResponseDTO;
+import com.wxcz.carpenter.pojo.entity.EcmBanner;
 import com.wxcz.carpenter.pojo.query.EcmAdSettingsQuery;
 import com.wxcz.carpenter.pojo.vo.EcmAdSettingsVO;
 import com.wxcz.carpenter.service.AdvertisingService;
@@ -21,6 +23,9 @@ import java.util.List;
 public class AdvertisingServiceImpl implements AdvertisingService {
     @Resource
     EcmAdSettingsDao ecmAdSettingsDao;
+    @Resource
+    EcmBannerDao ecmBannerDao;
+
 
     @Override
     public PageDTO ajaxList(EcmAdSettingsQuery ecmAdSettingsQuery) {
@@ -34,4 +39,16 @@ public class AdvertisingServiceImpl implements AdvertisingService {
         ecmAdSettingsDao.updateByPrimaryKeySelective(ecmAdSettingsVO);
         return ResponseDTO.ok();
     }
+
+    @Override
+    public ResponseDTO updateBannerAdSettings(EcmBanner ecmBanner) {
+        ecmBanner.setEcmBannerId(1);
+        return ResponseDTO.get(  ecmBannerDao.updateByPrimaryKeySelective(ecmBanner) == 1);
+    }
+
+    @Override
+    public ResponseDTO getBannerAd(EcmBanner ecmBanner) {
+        return ResponseDTO.ok( ecmBannerDao.selectByPrimaryKey(ecmBanner.getEcmBannerId()) );
+    }
+
 }
