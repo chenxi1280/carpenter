9331fdbae4ea3ee92d2c01eef48a3381064fd727
chenxi1280 <chenxi1280@outlook.com>
2021-06-08 10:29:42 +0800

    加入aop 日志

---
+++ src/main/java/com/wxcz/carpenter/common/annotation/LogAnnotation.java
@@ -0,0 +1,19 @@
+package com.wxcz.carpenter.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author by cxd
+ * @Classname LogAnnotation
+ * @Description TODO
+ * @Date 2021/6/8 10:10
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogAnnotation {
+
+
+}
